8076b6b04d73b04f92ce0a6d19db92b825ed2bc7
Samuel Stafford <69597989+samstafford626@users.noreply.github.com>
2023-05-15 14:32:53 -0500

    Create JS function getComputerChoice for determiniing the computers play (ROCK PAPER SCISSORS)

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+let randomNum = 0;
+    
+
+function getComputerChoice() {
+    randomNum = Math.floor(Math.random()*3)+1;
+    if (randomNum === 1) {
+        return "Rock"
+    } else if (randomNum === 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
